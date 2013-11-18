a08a5cdc1d48336e9c8e637a1620fff49a154e51
DrGupta <dr_ashish_1981@yahoo.com>
2013-11-18 18:50:26 +0100

    main and iniconfig

---
+++ Main.m
@@ -0,0 +1,15 @@
+% ImageClassificationPipeline
+% root : /export/home/gupta/Dropbox/CodeX/ImagePipeLine
+
+% -----------------------------
+% house-keeping and data-check
+% -----------------------------
+
+
+% -----------------------------
+% build image lists for training and testing
+% -----------------------------
+
+
+% -----------------------------
+% 
